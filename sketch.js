b59965049051fbc5adf4bb416e58a2966b2f93fa
aaronbrezel <35546183+aaronbrezel@users.noreply.github.com>
2019-01-24 12:10:15 -0500

    playing around with arcs

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ function setup() {
 	createCanvas(600,600); // make an HTML canvas element width x height pixels
 }
 
-var count = 0
 
 function draw() {
 	background(225);
@@ -21,7 +20,22 @@ function draw() {
 	else {
 		oClock = hour()-12
 	}
-	console.log(oClock)
+	
 	//Draw a blue triangle based on the hour
 	triangle(0,600,0,600-(50*oClock),50*oClock,600);
+
+	//Playing with arcs, DISREGUARD//
+	/*var secondHand = ((second()+1)/60)*TWO_PI
+	console.log(second())
+	if (secondHand < TWO_PI){
+		arc(300, 900, 100, 100, PI+PI/2, (PI+PI/2)+secondHand, PIE);
+	}
+	else {
+		console.log("TWO PI")
+		ellipse(300,900,100,100)
+	}	
+
+	arc(300,1100, 100,100, PI+PI/2,PI, PIE)
+	*/
+	
 }
